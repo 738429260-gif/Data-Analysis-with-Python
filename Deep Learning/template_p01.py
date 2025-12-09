6e2aa5f431576f62d8089d3696e9a90ef4e8791c
738429260-gif <738429260@qq.com>
2025-12-10 03:43:26 +0800

    Update template_p01.py

--- Deep Learning/template_p01.py
+++ Deep Learning/template_p01.py
@@ -22,8 +22,21 @@ def multiplicative_attention(decoder_hidden_state, encoder_hidden_states, W_mult
     return: np.array of shape (n_features_enc, 1)
         Final attention vector
     '''
-    # your code here
-    
+   def multiplicative_attention(decoder_hidden_state, encoder_hidden_states, W_mult):
+    """
+    decoder_hidden_state: np.array of shape (n_features_dec, 1)
+    encoder_hidden_states: np.array of shape (n_features_enc, n_states)
+    W_mult: np.array of shape (n_features_dec, n_features_enc)
+
+    return: np.array of shape (n_features_enc, 1)
+        Final attention vector
+    """
+    scores = decoder_hidden_state.T.dot(W_mult).dot(encoder_hidden_states)  # (1, n_states)
+
+    softmax_vector = softmax(scores)  # (1, n_states)
+
+    attention_vector = softmax_vector.dot(encoder_hidden_states.T).T  # (n_features_enc, 1)
+   
     return attention_vector
 
 def additive_attention(decoder_hidden_state, encoder_hidden_states, v_add, W_add_enc, W_add_dec):
@@ -37,6 +50,27 @@ def additive_attention(decoder_hidden_state, encoder_hidden_states, v_add, W_add
     return: np.array of shape (n_features_enc, 1)
         Final attention vector
     '''
-    # your code here
-    
+    def additive_attention(decoder_hidden_state, encoder_hidden_states,
+                       v_add, W_add_enc, W_add_dec):
+    """
+    decoder_hidden_state: np.array of shape (n_features_dec, 1)
+    encoder_hidden_states: np.array of shape (n_features_enc, n_states)
+    v_add: np.array of shape (n_features_int, 1)
+    W_add_enc: np.array of shape (n_features_int, n_features_enc)
+    W_add_dec: np.array of shape (n_features_int, n_features_dec)
+
+    return: np.array of shape (n_features_enc, 1)
+        Final attention vector
+    """
+    enc_part = W_add_enc.dot(encoder_hidden_states)
+    dec_part = W_add_dec.dot(decoder_hidden_state)
+
+    activation = np.tanh(enc_part + dec_part)
+
+    scores = v_add.T.dot(activation)
+
+    softmax_vector = softmax(scores)
+
+    attention_vector = softmax_vector.dot(encoder_hidden_states.T).T
+        
     return attention_vector
